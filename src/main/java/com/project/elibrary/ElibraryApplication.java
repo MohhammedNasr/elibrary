4b648673a677090e5147b595e7d60a5737eea01f
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-04-07 03:00:35 +0200

    first commit

---
+++ src/main/java/com/project/elibrary/ElibraryApplication.java
@@ -0,0 +1,13 @@
+package com.project.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/elibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
