8476b83e4ea551d22f1c071383ceb8e059f16a85
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-04-14 17:50:42 +0200

    fix

--- src/main/java/com/project/elibrary/controllers/UserController.java
+++ src/main/java/com/project/elibrary/controllers/UserController.java
@@ -1,18 +1,23 @@
 package com.project.elibrary.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.project.elibrary.dao.UserDao;
 import com.project.elibrary.models.User;
 import com.project.elibrary.repositories.UserRepo;
 
 @Controller
-@RequestMapping("/elibrary")
+@RequestMapping("/library")
 public class UserController {
 
     @Autowired
@@ -29,7 +34,7 @@ public class UserController {
     @PostMapping("save-user")
     public String saveUser(@ModelAttribute User user) {
         this.userRepo.save(user);
-        return "redirect:/elibrary/homepage";
+        return "redirect:/library/homepage";
 
     }
 
@@ -39,6 +44,29 @@ public class UserController {
         User newUser = new User();
         mav.addObject("user", newUser);
         return mav;
+    }
+
+
+
+    //for viewing users list (can be used by admins)
+    @Autowired
+    private UserDao userDao;
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        List<User> userList = userDao.getAllUsers();
+        model.addAttribute("users", userList);
+        model.addAttribute("isList", true);
+        return "profile.html";
+    }
+
+    //for finding a specific user (showing profile page)
+    @GetMapping("/users/{name}")
+    public String getUserByName(@PathVariable String name, Model model) {
+        User user = userDao.getUserByName(name);
+        model.addAttribute("user", user);
+        model.addAttribute("isList", false);
+        return "profile.html";
     }
 
 }
--- src/main/java/com/project/elibrary/repositories/UserRepo.java
+++ src/main/java/com/project/elibrary/repositories/UserRepo.java
@@ -1,11 +1,8 @@
 package com.project.elibrary.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.project.elibrary.models.User;
 
-@Repository
 public interface UserRepo extends JpaRepository<User, String> {
-
+    
 }
