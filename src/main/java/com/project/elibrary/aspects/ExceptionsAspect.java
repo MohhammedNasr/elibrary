4cc9d335573fb271c65580828768c5cdc13c4813
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-04-26 20:26:32 +0200

    exception aspect

---
+++ src/main/java/com/project/elibrary/aspects/ExceptionsAspect.java
@@ -0,0 +1,31 @@
+package com.project.elibrary.aspects;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExceptionsAspect {
+    @Pointcut("within(com.project.elibrary.controllers)")
+    void controllersPointCut(){};
+
+    @Around("controllersPointCut()")
+    Object exceptionHandler(ProceedingJoinPoint joinPoint){
+        try{
+            Object result = joinPoint.proceed();
+            return result;
+        } catch (Throwable e){
+            Map res = new HashMap<>();
+            res.put("website die",404);
+            return new ResponseEntity<>(res,HttpStatus.NOT_FOUND);
+        }
+    }
+}
