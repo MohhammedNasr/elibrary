5ae084432f75792fee80ac619ab894c24bf38a2d
Habiba Elganzoury <114189419+Habibaelganzoury1@users.noreply.github.com>
2023-04-28 16:27:26 +0300

    refactor

--- src/main/java/com/project/elibrary/ElibraryApplication.java
+++ src/main/java/com/project/elibrary/ElibraryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElibraryApplication.class, args); //lol
+		SpringApplication.run(ElibraryApplication.class, args); // lol
 	}
 
 }
