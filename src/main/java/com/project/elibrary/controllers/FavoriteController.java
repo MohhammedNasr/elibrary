be04edd919a790374b1e4b1aaa29acf96572d7da
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-04-30 03:14:45 +0300

    e

--- src/main/java/com/project/elibrary/controllers/FavoriteController.java
+++ src/main/java/com/project/elibrary/controllers/FavoriteController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +21,7 @@ public class FavoriteController {
     @Autowired
     private FavoriteService favoriteService;
 
+    //favorite list for a specific user
     @GetMapping("/{username}")
     public String getFavoritesByUsername(@PathVariable String username, Model model) {
         List<Favorite> favorites = favoriteService.getFavoritesByUsername(username);
