d655eb76ef831736408b861cff25de7d01df7235
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-05-16 22:44:19 +0300

    admin home page dynamic data and user list search function

--- src/main/java/com/project/elibrary/controllers/DynamicController.java
+++ src/main/java/com/project/elibrary/controllers/DynamicController.java
@@ -1,13 +1,16 @@
 package com.project.elibrary.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.project.elibrary.dto.BookDTO;
 import com.project.elibrary.models.User;
+import com.project.elibrary.repositories.BookRepository;
+import com.project.elibrary.repositories.UserRepo;
 
 @Controller
 @RequestMapping("/library")
@@ -25,11 +28,24 @@ public class DynamicController {
         return mav;
     }
 
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private BookRepository bookRepo;
+
     @GetMapping("adminHomePage")
-    public ModelAndView getAdminPage() {
+    public ModelAndView getAdminPage(@AuthenticationPrincipal User user) {
         ModelAndView mav = new ModelAndView("admin-home-page.html");
-        User newUser = new User();
-        mav.addObject("user", newUser);
+        long userCount = userRepo.count();
+        long donatedBooksCount = bookRepo.count();
+        String username = user.getUsername();
+        String profilePicture = user.getProfilePic();
+        //passing registered user count and donated books count
+        mav.addObject("userCount", userCount);
+        mav.addObject("donatedBooksCount", donatedBooksCount);
+        mav.addObject("username", username);
+        mav.addObject("profilePicture", profilePicture);
         return mav;
     }
 
