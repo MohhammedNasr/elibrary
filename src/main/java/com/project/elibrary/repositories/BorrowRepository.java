013c20f8fcd4920113ce2eb245ef1311dd40a94c
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-05-21 20:28:11 +0300

    end date borrow bug and code cleaning

--- src/main/java/com/project/elibrary/repositories/BorrowRepository.java
+++ src/main/java/com/project/elibrary/repositories/BorrowRepository.java
@@ -1,18 +1,9 @@
 package com.project.elibrary.repositories;
 
-import org.springframework.stereotype.Repository;
-
 import com.project.elibrary.models.Borrow;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-
-
-public interface BorrowRepository extends JpaRepository<Borrow, Long>
- {
+public interface BorrowRepository extends JpaRepository<Borrow, Long> {
     List<Borrow> findByUserID(Long userID);
 }
-
--- src/main/java/com/project/elibrary/services/BorrowService.java
+++ src/main/java/com/project/elibrary/services/BorrowService.java
@@ -1,15 +1,10 @@
 package com.project.elibrary.services;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.project.elibrary.models.Book;
 import com.project.elibrary.models.Borrow;
 import com.project.elibrary.repositories.BookRepository;
@@ -22,44 +17,38 @@ public class BorrowService {
     @Autowired
     private BorrowRepository borrowRepository;
 
-    
-
-    public void saveBorrow(Long bookId, Long userID, LocalDate startDate) {
+    public void saveBorrow(Long bookId, Long userID, LocalDate startDate, LocalDate endDate) {
         Optional<Book> optionalBook = bookRepository.findById(bookId);
         if (optionalBook.isPresent()) {
             Book book = optionalBook.get();
+            
+            // Get the book details
             String bookName = book.getTitle();
             String description = book.getDescription();
             String image = book.getThumbnailUrl();
-            List<String> author = book.getAuthors();
+            List<String> authors = book.getAuthors();
     
             if (book.getAvailability()) {
-                if (!book.isIsBorrowed()) {
-                    Borrow borrowed = new Borrow();
-                    borrowed.setBookId(bookId);
-                    borrowed.setBookName(bookName);
-                    borrowed.setAuthors(author);
-                    borrowed.setDescription(description);
-                    borrowed.setImage(image);
-                    borrowed.setUserID(userID);
-                    borrowed.setStartDate(startDate);
-                    // borrowed.setEndDate(endDate);
-                    borrowRepository.save(borrowed);
-    
-                    book.setAvailability(false); // Update availability to false
-                    bookRepository.save(book); // Save the updated book entity
-    
-                } else {
-                    throw new IllegalArgumentException("Book is currently borrowed");
-                }
+                Borrow borrowed = new Borrow();
+                borrowed.setBookId(bookId);
+                borrowed.setBookName(bookName);
+                borrowed.setAuthors(authors);
+                borrowed.setDescription(description);
+                borrowed.setImage(image);
+                borrowed.setUserID(userID);
+                borrowed.setStartDate(startDate);
+                borrowed.setEndDate(endDate);
+                borrowRepository.save(borrowed);
+                
+                book.setAvailability(false); // Update availability to false
+                bookRepository.save(book); // Save the updated book entity
             } else {
-                throw new IllegalArgumentException("Book isn't available now");
+                throw new IllegalArgumentException("Book is currently borrowed");
             }
         }
     }
     
-    public List<Borrow> getborrowedBooksByUserID(Long userID) 
-    {
+    public List<Borrow> getborrowedBooksByUserID(Long userID) {
         return borrowRepository.findByUserID(userID);
     }
 
