c9fbb7b8b2adbfe6f450a2f031216a5145a74edf
mohhammednasr <mohammed2011345@miuegypt.edu.eg>
2023-05-21 04:48:21 +0300

    admin donated books list frontend and admin nav bar

--- src/main/java/com/project/elibrary/services/BookService.java
+++ src/main/java/com/project/elibrary/services/BookService.java
@@ -6,9 +6,13 @@ import com.project.elibrary.models.Book;
 public interface BookService {
     List<Book> getAllBooks();
     List<Book> getReviewedBooks();
-    Book createBook(String title, String description, List<String> authors, String thumbnailUrl, Boolean availability, Boolean reviewed, Long userID);
     List<Book> getBooksByUserID(Long userID);
+
+    Book createBook(String title, String description, List<String> authors, String thumbnailUrl, Boolean availability, Boolean reviewed, Long userID);
+    
     void acceptBook(Long bookID);
     void rejectBook(Long bookID);
+    boolean adminEditBook(Long bookId, String thumbnail, String title, String description);
+    boolean adminDeleteBook(Long bookId);
 }
 
