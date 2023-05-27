008a6b844b088ab297b5388ab0adf590fd524e65
sarah801 <sarah2017578@miuegypt.edu.eg>
2023-05-27 14:55:54 +0300

    Cart Classes

---
+++ src/main/java/com/project/elibrary/controllers/CartController.java
@@ -0,0 +1,50 @@
+package com.project.elibrary.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import com.project.elibrary.models.Cart;
+import com.project.elibrary.models.User;
+import com.project.elibrary.services.CartService;
+
+@Controller
+@RequestMapping("/Cart")
+public class CartController
+ {
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addToCart(@RequestParam("bookName") String bookName,
+            @RequestParam("image") String image,
+            @RequestParam("price") double price,
+            @AuthenticationPrincipal User user) {
+        boolean bookAdded = cartService.addToCart(bookName, image, user, price);
+        if (bookAdded) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().body("Book is already in the cart.");
+        }
+    }
+
+    @GetMapping("/showCart")
+    public ModelAndView showCart(@AuthenticationPrincipal User user,Model model) {
+        Long userID = user.getId();
+        List<Cart> carts = cartService.getAllCartItems(userID);
+        ModelAndView mav =new ModelAndView("Cart"); 
+      
+        mav.addObject("cart",carts); 
+      return  mav ; 
+       
+    }
+   
+}
---
+++ src/main/java/com/project/elibrary/services/CartService.java
@@ -0,0 +1,54 @@
+package com.project.elibrary.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.elibrary.models.Cart;
+import com.project.elibrary.models.User;
+import com.project.elibrary.repositories.CartRepository;
+import com.project.elibrary.repositories.UserRepo;
+
+@Service
+public class CartService {
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private UserRepo userRepository;
+
+    public boolean addToCart(String bookName, String image, User user, double price) {
+        Long userID = user.getId();
+        List<Cart> CartOptional = cartRepository.findByUser_IdAndBookName(userID, bookName);
+        if (CartOptional.isEmpty()) {
+            Cart cart = new Cart();
+            cart.setBookName(bookName);
+            cart.setThumbnailUrl(image);
+            cart.setUser(user);
+            cart.setPrice(price);
+            cart.setQuantity(1);
+            cart.setTotalPrice(cart.getPrice() * cart.getQuantity());
+            cartRepository.save(cart);
+            user.addToCart(cart);
+            userRepository.save(user);
+            cartRepository.save(cart);
+            return true; // Book added to cart successfully
+        } else {
+            return false; // Book is already in the cart
+        }
+    
+    
+}
+public List<Cart> getAllCartItems(Long userId) 
+{
+    return cartRepository.findByUser_Id(userId);
+}
+}
+/*else {
+            Cart existingCartItem = cartItems.get(0);
+            existingCartItem.setQuantity(existingCartItem.getQuantity() + 1);
+            existingCartItem.setTotalPrice(existingCartItem.getPrice().multiply(new BigDecimal(existingCartItem.getQuantity())));
+            cartRepository.save(existingCartItem);
+            return false; // Book is already in the cart
+        }    */
